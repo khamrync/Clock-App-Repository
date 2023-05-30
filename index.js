9da2c93c8ac9d613593ba60d966df19071288359
khamrync <khamryn.turner@gmail.com>
2023-05-29 20:03:18 -0600

    Final fixes including fix for updateTime function

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-//Los Angeles//
-function updateTime() {
-    let losAngelesElement = document.querySelector("#los-angeles");
-    if (losAngelesElement) {
-        let losAngelesDateElement = losAngelesElement.querySelector(".date")
-        let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-        let losAngelesTime = moment().tz("America/Los_Angeles");
+function updateTime(cityTimeZone) {
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
 
-        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-        losAngelesTimeElement.innerHTML = losAngelesTime.format(`LTS`);
-    };
-};
-
-function updateCity(event) {
-    let cityTimeZone = event.target.value;
-    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML = `
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
     <div class="city">
-            <div>
-                <h2>${cityName}</h2>
-                <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-            </div>
-            <div class="time">${cityTime.format(`LTS`)}</div>
-        </div>
-    `;
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format(`LTS`)}</div>
+    </div>
+  `;
 }
 
-updateTime();
-setInterval(updateTime, 1000);
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  updateTime(cityTimeZone);
+
+  setInterval(function () {
+    updateTime(cityTimeZone);
+  }, 1000);
+}
 
 let citiesSelect = document.querySelector("#time-zone");
 citiesSelect.addEventListener("change", updateCity);
